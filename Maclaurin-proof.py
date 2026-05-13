a7a60db18033446e11b003ee5cf8835313b72c86
Howard (Ching-Chia) Kuo <howard900126@gmail.com>
2026-05-13 11:06:30 +0800

    Update Maclaurin-proof.py

--- Maclaurin-proof.py
+++ Maclaurin-proof.py
@@ -30,6 +30,21 @@ def taylor_expansion_exp(x,num_terms):
 
 target_x = 2
 terms_to_compute = 10
-taylor_expansion_exp(target_x,terms_to_compute )
+
+#taylor_expansion_exp(target_x,terms_to_compute )
+#--- Approximating e^2 using 10 terms ---
+#n = 0  | Term: 1.00000    | Current Sum: 1.00000
+#n = 1  | Term: 2.00000    | Current Sum: 3.00000
+#n = 2  | Term: 2.00000    | Current Sum: 5.00000
+#n = 3  | Term: 1.33333    | Current Sum: 6.33333
+#n = 4  | Term: 0.66667    | Current Sum: 7.00000
+#n = 5  | Term: 0.26667    | Current Sum: 7.26667
+#n = 6  | Term: 0.08889    | Current Sum: 7.35556
+#n = 7  | Term: 0.02540    | Current Sum: 7.38095
+#n = 8  | Term: 0.00635    | Current Sum: 7.38730
+#n = 9  | Term: 0.00141    | Current Sum: 7.38871
+#---------------------------------------------
+#Final Approximation: 7.3887125
+#Exact math.exp(2):   7.3890561
     
    
